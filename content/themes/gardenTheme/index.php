a61c89a68e041600024b0dad787010a2428d2acb
Jay DeBard <debard@gmail.com>
2015-06-20 19:52:03 -0400

    Background image finally working

--- content/themes/gardenTheme/index.php
+++ content/themes/gardenTheme/index.php
@@ -12,6 +12,26 @@
  */
 get_header(); ?>
 
-
+<div class="jumbotron">
+	<div class="row">
+		<div class="centered fourteen columns">
+			<h4 align="center" class="xbold">Welcome to FFXIV Gardening</h4>
+			<p align="center" class="xbold">The FFXIV Gardening Database is a collection of everything related to gardening in Final Fantasy XIV: A Realm Reborn. It was created to organize and visualize the results obtained from crossbreeding.</p>
+		</div>
+		<div class="centered twelve columns" align="center">
+			<div class="medium danger btn">
+				<a href="#">View Update Archive</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+<div class="twelve columns">
+<p>Left</p>
+</div>
+<div class="four columns">
+<p align="right">Right</p>
+</div>
+</div>
 
 <?php get_footer(); ?>
